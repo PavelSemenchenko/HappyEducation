67e50b1dbf09499ccde8d4c50d8653f51cb9ad62
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-14 21:39:23 +0200

    PHE-006
    corrected register UI
    add link 2 login

--- HappyEducation/VC/SignUpVC.swift
+++ HappyEducation/VC/SignUpVC.swift
@@ -8,6 +8,29 @@
 import Foundation
 import UIKit
 
-class SignUpVC: UIViewController {
+class SignUpVC: UIViewController, BaseAuthentiticationVC {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: EmailTextField!
+    @IBOutlet weak var passwordTextField: PasswordTextField!
+    @IBOutlet weak var errorEmailLabel: UILabel!
+    @IBOutlet weak var errorPasswordLabel: UILabel!
+    
+    let authenticationService: AuthentiticationService = FirebaseAuthentiticationService()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func signUpButtonclicked(_ sender: Any) {
+    }
+    
+    @IBAction func signInButtonClicked(_ sender: Any) {
+        guard let login = self.storyboard?.instantiateViewController(withIdentifier: "SignIn") as? SignInVC else { return
+            
+        }
+        self.navigationController?.pushViewController(login, animated: true)
+    }
     
 }
