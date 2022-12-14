ea900c3e72b972b960f51d45d18aad4c0c74005e
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-14 21:56:44 +0200

    PHE-006
    done signUP
    
    Closes PHE-006

--- HappyEducation/VC/SignUpVC.swift
+++ HappyEducation/VC/SignUpVC.swift
@@ -24,6 +24,43 @@ class SignUpVC: UIViewController, BaseAuthentiticationVC {
     }
     
     @IBAction func signUpButtonclicked(_ sender: Any) {
+        validateFields()
+        
+        let optionalEmail = emailTextField.text
+        guard let email = optionalEmail, email.contains("@") else {
+            emailTextField.layer.borderColor = UIColor.red.cgColor
+            errorEmailLabel.text = "Enter correcnt email"
+            errorEmailLabel.isHidden = false
+            return
+        }
+        emailTextField.layer.borderColor = UIColor.green.cgColor
+        errorEmailLabel.isHidden = true
+        
+        let optionalPassword = passwordTextField.text
+        guard let password = optionalPassword, password.count >= 6 else {
+            passwordTextField.layer.borderColor = UIColor.red.cgColor
+            errorPasswordLabel.text = "Enter password lager then 6"
+            errorPasswordLabel.isHidden = false
+            return
+        }
+        
+        passwordTextField.layer.borderColor = UIColor.green.cgColor
+        errorPasswordLabel.isHidden = true
+        
+        authenticationService.signUp(email: email, password: password) { errorMessage in
+            if let message = errorMessage {
+                let alert = UIAlertController(title: "Happe Education",
+                                              message: message,
+                                              preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "Fine", style: UIAlertAction.Style.default))
+                self.present(alert, animated: true)
+            } else {
+                guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? HomeVC else {
+                    return
+                }
+                self.navigationController?.pushViewController(homeVC, animated: true)
+            }
+        }
     }
     
     @IBAction func signInButtonClicked(_ sender: Any) {
