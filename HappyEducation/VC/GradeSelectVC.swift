7c2ab6980b6ad3012a79f8cf8b1a5d2a562dee90
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-15 23:25:23 +0200

    PHE-008
    thinking about logic of grades

--- HappyEducation/VC/GradeSelectVC.swift
+++ HappyEducation/VC/GradeSelectVC.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class GradeSelectVC: UIViewController {
     
-    
+    func validateGrades() {
+       
+    }
     
 }
+
+
+/*
+ если нажат уровень - borderColor.blue.cgColor
+ и все уровни в дефолт
+ 
+ ?? выбор только одного уровня ( смена со сбросом)
+ 
+ если выбран уровень is hiden = false
+ 
+ */
