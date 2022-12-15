812beab36bca1db988c634d44616df85435e6d1f
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-15 21:19:54 +0200

    keyboardBug
    - try to fix scroll by scrollView

--- HappyEducation/VC/SignInVC.swift
+++ HappyEducation/VC/SignInVC.swift
@@ -9,7 +9,8 @@ import Foundation
 import UIKit
 
 class SignInVC: UIViewController, BaseAuthentiticationVC, UITextFieldDelegate {
-        
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var emailTextField: EmailTextField!
     @IBOutlet weak var passwordTextField: PasswordTextField!
     @IBOutlet weak var errorPasswordLabel: UILabel!
@@ -21,7 +22,32 @@ class SignInVC: UIViewController, BaseAuthentiticationVC, UITextFieldDelegate {
         super.viewDidLoad()
         emailTextField.delegate = self
         passwordTextField.delegate = self
+        
+        // keyboard Hiding
+        registerForKeyboardNotifications()
     }
+    deinit {
+        removeKeyboardNotifications()
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func kbWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo
+        let kbFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        scrollView.contentOffset = CGPoint(x: 0.0, y: kbFrameSize.height)
+    }
+    @objc func kbWillHide() {
+        scrollView.contentOffset = CGPoint.zero
+    }
+    
     
     func signIn() {
         guard let email = emailTextField.validateEmailTextField(errorLabel: errorEmailLabel) else {
