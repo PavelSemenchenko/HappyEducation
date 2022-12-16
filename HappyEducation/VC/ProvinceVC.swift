2397ff3ae321f3db6da9629529efdb9ab2698d01
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-16 20:22:17 +0200

    PHE-009 UI province

---
+++ HappyEducation/VC/ProvinceVC.swift
@@ -0,0 +1,13 @@
+//
+//  ProvinceVC.swift
+//  HappyEducation
+//
+//  Created by Pavel Semenchenko on 16.12.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProvinceVC: UIViewController {
+    
+}
