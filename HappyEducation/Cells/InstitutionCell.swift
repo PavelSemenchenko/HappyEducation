bfc0ff2426ae4fdc17c94454c5ad9aeb795262ce
PavelSemenchenko <imagemakerspa@icloud.com>
2022-12-29 14:32:43 +0200

    adding collage UI

---
+++ HappyEducation/Cells/InstitutionCell.swift
@@ -0,0 +1,17 @@
+//
+//  InstitutionCell.swift
+//  HappyEducation
+//
+//  Created by MakBook on 29.12.2022.
+//
+
+import UIKit
+
+class InstitutionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
