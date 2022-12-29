4769c71b1a9d4decba8d86235ad7cec8f72b51ce
Pavel Semenchenko <111786899+PavelSemenchenko@users.noreply.github.com>
2022-12-29 19:31:38 +0200

    [PHE-012] - [UI] Teachers list

---
+++ HappyEducation/Cells/TeacherCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherCell.swift
+//  HappyEducation
+//
+//  Created by MakBook on 29.12.2022.
+//
+
+import UIKit
+
+class TeacherCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
