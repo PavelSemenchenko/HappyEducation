d0982b4cc539d756df2f469747ea6e23e4c1cd78
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-29 22:13:04 +0200

    error creating teachers

--- HappyEducation/Cells/TeacherCell.swift
+++ HappyEducation/Cells/TeacherCell.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class TeacherCell: UICollectionViewCell {
 
+    @IBOutlet weak var teacherImageView: UIImageView!
+    @IBOutlet weak var teacherNameLabel: UILabel!
+    @IBOutlet weak var teacherSubjectLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    var teacher: Teacher! {
+        didSet {
+            prepareForReuse()
+        }
+    }
+    var teacherRepository: TeachersRepository!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        teacherNameLabel.text = teacher.name
+        teacherSubjectLabel.text = teacher.subject
     }
 
 }
--- HappyEducation/VC/HomeVC.swift
+++ HappyEducation/VC/HomeVC.swift
@@ -8,5 +8,52 @@
 import Foundation
 import UIKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var userTableView: UITableView!
+    @IBOutlet weak var teachersFilterButton: UIButton!
+    @IBOutlet weak var teachersCollectionView: UICollectionView!
+    @IBOutlet weak var institutionsFilterButton: UIButton!
+    @IBOutlet weak var institutionsCollectionView: UICollectionView!
+    
+    let teachersRepository: TeachersRepository = FirebaseTeachersRepository()
+    var teachers: [Teacher] = []
+    let authenticationService: AuthentiticationService = FirebaseAuthentiticationService()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // hide back bar
+        navigationItem.setHidesBackButton(true, animated: true)
+        // register cells
+        userTableView.register(UINib(nibName: "HelloUserCel", bundle: nil)
+                               , forCellReuseIdentifier: "HelloUserCel")
+        teachersCollectionView.register(UINib(nibName: "TeachersCell", bundle: nil)
+                                        , forCellWithReuseIdentifier: "TeachersCell")
+        institutionsCollectionView.register(UINib(nibName: "InstitutionCell", bundle: nil)
+                                            , forCellWithReuseIdentifier: "InstitutionCell")
+    }
+    /*
+     // tableView User
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return user?.count
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+     }
+     */
+    // collectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return teachers.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TeachersCell", for: indexPath) as! TeacherCell
+        
+        cell.teacher = teachers[indexPath.row]
+        cell.teacherRepository = TeachersRepository
+        
+        return cell
+        
+    }
 }
