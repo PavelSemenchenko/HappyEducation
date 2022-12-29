edcaa7d6273617a5cbd2d95d3e56e9b27b607caf
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-29 20:53:03 +0200

    adding user repository

--- HappyEducation/Repositories/UserProfilesRepository.swift
+++ HappyEducation/Repositories/UserProfilesRepository.swift
@@ -11,8 +11,56 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 struct UserProfile: Codable {
+    @DocumentID var id: String?
     let name: String
     let email: String
     let password: String
 }
 
+protocol UserProfilesRepository {
+    func getAll()
+    func getOne(userId: String, completion: @escaping (UserProfile?) -> Void)
+    func create(profile: UserProfile)
+    func delete()
+}
+
+class FirebaseUsesrProfilesRepository: UserProfilesRepository {
+    func getAll() {
+        
+    }
+    
+    func getOne(userId: String, completion: @escaping (UserProfile?) -> Void) {
+        Firestore.firestore().collection("profiles")
+            .document(userId)
+            .getDocument(as: UserProfile.self) { snapshot in
+            switch(snapshot) {
+            case .success(let profile):
+                completion(profile)
+            case .failure(_):
+                completion(nil)
+            }
+        }
+    }
+    
+    func create(profile: UserProfile) {
+        var userProfile = profile
+        guard let currentUserId = Auth.auth().currentUser?.uid else {
+            fatalError("Only after authentication")
+        }
+        userProfile.id = currentUserId
+        try? Firestore.firestore()
+            .collection("profiles")
+            .document(currentUserId)
+            .setData(from: userProfile)
+    }
+    
+    func delete() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    
+}
--- HappyEducation/Services/Authetitication.swift
+++ HappyEducation/Services/Authetitication.swift
@@ -11,9 +11,13 @@ import FirebaseAuth
 protocol AuthentiticationService {
     func signIn(email: String, password: String, completion: @escaping (String?) -> Void)
     func signUp(email: String, password: String, completion: @escaping (String?) -> Void)
+    func forgotPassword(email: String, completion: @escaping (String?) -> Void)
+    func isAuthenticated() -> Bool
+    func logOut()
 }
 
 class FirebaseAuthentiticationService: AuthentiticationService {
+      
     func signIn(email: String, password: String, completion: @escaping (String?) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if result?.user != nil {
@@ -34,6 +38,15 @@ class FirebaseAuthentiticationService: AuthentiticationService {
             }
         }
     }
+    func forgotPassword(email: String, completion: @escaping (String?) -> Void) {
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            completion(error?.localizedDescription)
+        }
+    }
+    func logOut() {
+        try? Auth.auth().signOut()
+    }
+    
     func isAuthenticated() -> Bool {
         let hasUser = Auth.auth().currentUser != nil
         return hasUser 
