b72b7ca2a9b6e5962df03e5675a5e7666a741fa8
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-12-29 22:30:52 +0200

    pass with institutions

--- HappyEducation/Repositories/InstitutionsRepository.swift
+++ HappyEducation/Repositories/InstitutionsRepository.swift
@@ -18,3 +18,27 @@ struct Institution: Codable {
     let voted: Int
     let subject: String
 }
+protocol InstitutionRepository {
+    func getAll(completion: @escaping ([Institution]) -> Void)
+}
+class FirebaseInstitutionRepository: InstitutionRepository {
+    lazy var institutionsCollection: CollectionReference = {
+        return Firestore.firestore().collection("institutions")
+    }()
+    func getAll(completion: @escaping ([Institution]) -> Void) {
+        institutionsCollection.getDocuments { snapshot, _ in
+            guard let docs = snapshot?.documents else {
+                completion([])
+                return
+            }
+            var institutions: [Institution] = []
+            for doc in docs {
+                guard let institution = try? doc.data(as: Institution.self) else {
+                    continue
+                }
+                institutions.append(institution)
+            }
+            completion(institutions)
+        }
+    }
+}
