29767256bf3dbfacc11c150bc8e783ff2ef20974
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-01-11 20:45:29 +0200

    institution adding code - import Alamofire - added shadows to View

--- HappyEducation/Cells/InstitutionCell.swift
+++ HappyEducation/Cells/InstitutionCell.swift
@@ -6,20 +6,25 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class InstitutionCell: UICollectionViewCell {
 
     @IBOutlet weak var institutionNameLabel: UILabel!
     @IBOutlet weak var institutionImageVew: UIImageView!
-    
     @IBOutlet weak var institutionStarsLabel: UILabel!
-    
     @IBOutlet weak var institutionDescriptionTextView: UITextView!
     @IBOutlet weak var institutionVotesLabel: UILabel!
     @IBOutlet weak var institutionSubjectLabel: UILabel!
+    @IBOutlet weak var institutionView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        institutionView.layer.shadowOpacity = 1
+        institutionView.layer.shadowRadius = 8.0
+        institutionView.layer.shadowOffset = CGSize.zero
+        institutionView.layer.shadowColor = UIColor.systemGray4.cgColor
+        institutionView.layer.cornerRadius = 10
         // Initialization code
     }
 
