b85ac24ca8bacc7ca6e93eef11654a7faa733cc7
Pavel Semenchenko <imagemakerspa@icloud.com>
2023-01-16 21:39:51 +0200

    UI need correct scroll screen

--- HappyEducation/VC/AddTeacherVC.swift
+++ HappyEducation/VC/AddTeacherVC.swift
@@ -11,12 +11,16 @@ import UIKit
 class AddTeacherVC: UIViewController {
     
     @IBOutlet weak var addTeacherView: UIView!
-    /*
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        
+        /*
         addTeacherView.layer.shadowOpacity = 1
         addTeacherView.layer.shadowRadius = 12.0
         addTeacherView.layer.shadowOffset = CGSize.zero
         addTeacherView.layer.shadowColor = UIColor.gray.cgColor
-    }*/
+         */
+    }
 }
